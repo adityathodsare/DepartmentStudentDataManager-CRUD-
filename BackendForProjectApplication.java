89921fa8b321d8a32fc964b1eb1d6c8ad6440eb8
aditya thodsare <thodsareaditya@gmail.com>
2024-11-03 10:43:31 +0530

    Add files via upload

---
+++ BackendForProjectApplication.java
@@ -0,0 +1,13 @@
+package net.DBMSprojectAny.backendForProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendForProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendForProjectApplication.class, args);
+	}
+
+}
---
+++ WebConfig.java
@@ -0,0 +1,24 @@
+package net.DBMSprojectAny.backendForProject;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Allow all endpoints
+                .allowedOrigins("http://127.0.0.1:3000") // Change this to your frontend URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true);
+    }
+}
+
